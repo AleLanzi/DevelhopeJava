30f012880390a5cb5610318994c067e65c72d330
AleLanzi <119108870+AleLanzi@users.noreply.github.com>
2022-12-13 16:18:43 +0100

    Add files via upload

---
+++ esercizio9/src/it/develhope/polimorfismo/AnimalStart.java
@@ -0,0 +1,12 @@
+package it.develhope.polimorfismo;
+
+public class AnimalStart {
+
+        public static void main(String[] args) {
+            Animal lion = new Animal("Lion");
+            lion.animalSound();
+            lion.animalSound("high");
+
+        }
+    }
+
